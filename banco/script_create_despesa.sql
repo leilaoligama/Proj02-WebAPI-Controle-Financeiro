7b86e78bb950c80890d680a5abcb77d8a9d17424
Leila <leilagama@gmail.com>
2023-02-07 09:25:56 -0300

    Criação do banco de dados "Controle Financeiro" e das tabelas "Despesa" e "Receita"

---
+++ banco/script_create_despesa.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].Despesa
+(
+	[DespesaId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Descricao] VARCHAR(100) NULL, 
+    [Valor] DECIMAL NOT NULL, 
+    [Data] DATETIME NOT NULL
+)
---
+++ banco/script_create_receita.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].Receita
+(
+	[ReceitaId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Descricao] VARCHAR(100) NULL, 
+    [Valor] DECIMAL NOT NULL, 
+    [Data] DATETIME NOT NULL
+)

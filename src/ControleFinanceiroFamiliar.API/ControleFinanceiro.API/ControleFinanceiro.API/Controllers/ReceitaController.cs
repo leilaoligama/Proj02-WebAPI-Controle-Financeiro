35df10a4a09fff467c475578e72ba3e66e49c663
Leila <leilagama@gmail.com>
2023-02-11 22:58:25 -0300

    Criação da controller ObterReceitas.

--- src/ControleFinanceiroFamiliar.API/ControleFinanceiro.API/ControleFinanceiro.API/Controllers/ReceitaController.cs
+++ src/ControleFinanceiroFamiliar.API/ControleFinanceiro.API/ControleFinanceiro.API/Controllers/ReceitaController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ControleFinanceiro.Business.Models;
+using ControleFinanceiro.Data.Context;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ControleFinanceiro.API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ReceitaController : ControllerBase
     {
+        private readonly MeuDbContext _dbContext;
+
+        public ReceitaController(MeuDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Receita>> ObterRceitas()
+        {
+            return await _dbContext.Receitas.ToListAsync();
+
+        }
 
     }
 }
